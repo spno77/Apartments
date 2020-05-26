f68ec6a2be5a31d17fe18507d2d7cd33ac27d64f
spno77 <pilonoti@yahoo.com>
2020-05-26 18:28:13 +0300

    Rest-Auth additions

--- config/config/urls.py
+++ config/config/urls.py
@@ -9,9 +9,12 @@ from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('api/v1/',include('users.urls')),
     path('api/v1/',include('apartments.urls')),
+	
 	path('api-auth/', include('rest_framework.urls')),
+	path('api/v1/rest-auth/', include('rest_auth.urls')),
 
 	path('api-token-auth/', obtain_jwt_token),
 	path('api-token-refresh/', refresh_jwt_token),
