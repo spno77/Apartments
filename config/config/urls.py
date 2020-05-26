e13226884c4719177b1096199416e9e335a50963
spno77 <pilonoti@yahoo.com>
2020-05-26 18:11:33 +0300

    jwt addition

--- config/config/urls.py
+++ config/config/urls.py
@@ -5,11 +5,17 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('users.urls')),
     path('api/v1/',include('apartments.urls')),
 	path('api-auth/', include('rest_framework.urls')),
+
+	path('api-token-auth/', obtain_jwt_token),
+	path('api-token-refresh/', refresh_jwt_token),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
